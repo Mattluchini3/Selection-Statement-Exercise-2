ebddd9a58ae43c5913d797d1231599049e91aa81
Matthew Luchini <luch@Matthews-MBP.lan>
2023-02-03 12:57:49 -0500

    Selections2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a tough subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! is haven't taken that subject before. {subject} sounds fun!");
+                    break;
+            }
         }
     }
 }
